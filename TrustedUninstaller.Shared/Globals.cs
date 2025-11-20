2d15e5da9ff014dc96432798a593d6e7fdd0b885
Styris <styris@styris.net>
2025-11-20 09:31:16 -0800

    Update to v0.8.3

--- TrustedUninstaller.Shared/Globals.cs
+++ TrustedUninstaller.Shared/Globals.cs
@@ -15,7 +15,7 @@ namespace TrustedUninstaller.Shared
 {
     public class Globals
     {
-        public const string CurrentVersion = "0.8.2";
+        public const string CurrentVersion = "0.8.3";
         public static readonly VersionNumber CurrentVersionNumber = VersionNumber.GetVersionNumber(CurrentVersion);
     }
     [Serializable]
